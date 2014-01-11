fcf903363bac080a0bc301472a55043ace8cb053
fukayatsu <fukayatsu@gmail.com>
2014-01-12 00:59:26 +0900

    first commit

---
+++ watcher.rb
@@ -0,0 +1,49 @@
+require 'json'
+require 'faraday'
+require 'faraday_middleware'
+
+class Watcher
+  def initialize
+    @conn = Faraday.new(url: 'http://ucchusma.herokuapp.com/api/v1') do |faraday|
+      faraday.adapter Faraday.default_adapter
+      faraday.request  :json
+      faraday.response :json
+    end
+
+    ip_address = Socket.ip_address_list.find(&:ipv4_private?).ip_address
+    resp = @conn.put do |req|
+      req.url 'info'
+      req.headers['Content-Type'] = 'application/json'
+      req.body = { message: ip_address, token: ENV['TOKEN'] }.to_json
+    end
+  end
+
+  def watch
+    @count = 0
+    @history ||= []
+
+    loop do
+      sleep 1
+
+      @history << `cat /sys/class/gpio/gpio4/value`.to_i
+      @count += 1
+      next if @history.length < 10
+
+      @count = 0 if @count >= 60
+      @history.shift if @history.length > 180
+      next unless @count % 10 == 0
+
+      status = @history.count { |v| v == 1 } > 5 ? 'occupied' : 'vacant'
+
+      resp = @conn.put do |req|
+        req.url 'rooms/1'
+        req.headers['Content-Type'] = 'application/json'
+        req.body = { status: status, token: ENV['TOKEN'] }.to_json
+      end
+    end
+  end
+end
+
+wacher = Watcher.new
+wacher.watch
+
